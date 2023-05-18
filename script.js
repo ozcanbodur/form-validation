e00c1bc9f717d7a34c760d6ddf86755f4c825b5d
Ozcan Bodur <104697381+ozcanbodur@users.noreply.github.com>
2023-05-18 10:56:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const repassword = document.getElementById('repassword');
+
+
+function error(input, message){
+    input.className =  'form-control is-invalid';
+    const div = input.nextElementSibling;
+    div.innerText = message;
+    div.className = 'invalid-feedback';
+
+}
+function success(input){
+    input.className =  className = 'form-control is-valid';
+}
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+function checkReqired(inputs){
+    inputs.forEach(function(input){
+        if(input.value === ''){
+            error(input, `${input.id} is required.`);
+        }
+        else{
+            success(input);
+        }
+    })
+   
+}
+
+form.addEventListener('submit', function(e){
+
+    e.preventDefault();
+   checkReqired([username, email,password,repassword]);
+});
